b34145a0a1011737564a23e31a3227bbfe8f4c66
Jonathan Pepin <jypepin@gmail.com>
2018-09-25 10:35:04 +0200

    react-require so Storybook doesnt complain React is not defined

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-
 export default props => {
   return (<div>
     <div className='btn'>{props.children}<span>salut</span></div>
     <style jsx>{`
       div {
         color: red;
+        display: flex;
         span {
           color: blue;
         }
