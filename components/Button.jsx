57472520c76cea57754f29f2eff057a8180d79fc
Jonathan Pepin <jypepin@gmail.com>
2018-09-25 13:24:48 +0200

    Allow import of css and scss files in Next and Storybook

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,6 +1,9 @@
+import './Button.scss'
+import './Button.css'
+
 export default props => {
   return (<div>
-    <div className='btn'>{props.children}<span>salut</span></div>
+    <div className='btn bold'>{props.children}<span>salut</span></div>
     <style jsx>{`
       div {
         color: red;
